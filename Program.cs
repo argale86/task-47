c6d999f529b0e362a3ccdbcbc9b2ae2ab21e5ad9
Kirill <shadow986@mail.ru>
2023-03-05 01:09:04 +0300

    Задание 47 изменение 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
 ﻿//сгенерировать двумерный массив вещественных чисел размером m на n элементов
-Console.WriteLine("Введите кол-во элементов в массиве:");
+Console.WriteLine("Введите кол-во элементов по горизонтали в массиве:");
 int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во элементов по вертикали в массиве:");
 int n = Convert.ToInt32(Console.ReadLine());
 float [ , ] mas = new float [m,n];
 Random rnd = new Random();
-for (int count1 = 0; count1 < m; count1++)
-{
-    for (int count2 = 0; count2 < n; count2++)
-    {
-        mas[count1,count2] = rnd.Next(-32654, 32654);
-    }
-}
 for (int count1 = 0; count1 < m; count1++)
 {
     for (int count2 = 0; count2 < n; count2++)
@@ -20,3 +14,4 @@ for (int count1 = 0; count1 < m; count1++)
     }
     Console.WriteLine();
 }
+
